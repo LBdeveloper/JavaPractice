31b780478d1472e76fef67a9dfe48f3cc7e9beee
LBdeveloper <LB_016@163.com>
2017-02-28 08:56:35 +0800

    2017-2-28

--- oop/MarsStroll/src/Main.java
+++ oop/MarsStroll/src/Main.java
@@ -1,16 +1,27 @@
 import util.vehicle.*;
 import signal.Signal;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Vehicle marsCar = new MarsCar("夸父",5,"蓝色");
-		Signal s = new Signal("wd");
-		marsCar.printPosition();
-		marsCar.doMove(s);
-		marsCar.printPosition();
+		// Signal s = new Signal("wd");
+		// marsCar.printPosition();
+		// marsCar.doMove(s);
+		// marsCar.printPosition();
 
-		
-
+		while (true) {
+			marsCar.printPosition();
+			Scanner sc = new Scanner(System.in);
+			String rawSignal = sc.next();
+			Signal s = new Signal(rawSignal);
+			marsCar.doMove(s);
+			marsCar.printPosition();
+			if("stop".equals(rawSignal)) {
+				return ;
+			}
+		}
+	
 		
 	}
 }
